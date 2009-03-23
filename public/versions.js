ee567ff870c192f6fa511cb90e23caa462444e41
Volodya Kolesnikov <voloko@gmail.com>
2009-03-23 13:15:47 +0300

    Version changer

--- public/versions.js
+++ public/versions.js
@@ -1,5 +1,5 @@
 var versions = [
-{name: 'v2.1.2', size: '2.32', browse: '/doc/v2.1.2/', download: '/doc/v2.1.2/rdoc.zip'}
-{name: 'v2.2.2', size: '2.32', browse: '/doc/v2.2.2/', download: '/doc/v2.2.2/rdoc.zip'}
+{name: 'v2.1.2', size: '2.10', browse: '/doc/v2.1.2/', download: '/doc/v2.1.2/rdoc.zip'},
+{name: 'v2.2.2', size: '2.18', browse: '/doc/v2.2.2/', download: '/doc/v2.2.2/rdoc.zip'},
 {name: 'v2.3.2', size: '2.32', browse: '/doc/v2.3.2/', download: '/doc/v2.3.2/rdoc.zip'}
 ]
