e8a61373c95c0f0f4b5d6cb33798f52cdbdc7be8
Volodya Kolesnikov <voloko@gmail.com>
2009-04-06 10:46:24 +0400

    better handling of /rdoc.zip /index.html

--- sdoc-site.rb
+++ sdoc-site.rb
@@ -2,17 +2,18 @@ $:.unshift ::File.expand_path(::File.join(::File.dirname(__FILE__), 'lib/'))
 require "sdoc_site"
 require "sdoc_site/builds"
 
-get '/doc/:build/*' do
+get %r{/doc/([^/]+)/([^/]*)} do
   begin
     
     builds = SDocSite::Builds.new(File.join('public', 'doc'))
-    merged_build = SDocSite::Builds::MergedBuild.from_str params[:build]
+    merged_build = SDocSite::Builds::MergedBuild.from_str params["captures"][0]
     available_build = builds.merged_build(merged_build)
     
     public_dir = File.join('public', 'doc')
     target = File.join public_dir, merged_build.to_s
     if File.exists?(target)
-      return haml(:building, :locals => {:build => params[:build]})
+      pass if File.exists?(File.join(target, 'rdoc.zip'))
+      return haml(:building, :locals => {:build => params["captures"][0]})
     end
     
     if available_build
@@ -24,9 +25,9 @@ get '/doc/:build/*' do
     a = SDocSite::Automation.new File.expand_path(public_dir)
     a.merge_builds merged_build
     a.generate_index
-    redirect "/doc/#{merged_build}/"
+    redirect "/doc/#{merged_build}/#{params["captures"][1]}"
     
   rescue Exception => e
-    e.to_s
+    e.to_s + params.inspect
   end
 end
